fe8e29e0c39b3843a5a7a1e46f1a0e277759e29f
David Knott <davek@communicraft.com>
2021-12-17 13:42:56 +0000

    changed line endings

--- build-all.sh
+++ build-all.sh
@@ -2,17 +2,18 @@
 
 set -e 
 
-docker build --rm -t cespi/php-5.3:cli .
-docker build --rm -t cespi/php-5.3:apache apache/
-docker build --rm -t cespi/php-5.3:fpm fpm/
+#docker build --rm -t docker.dublin.communicraft.com:5000/php-5.3:cli .
+docker build --rm -t docker.dublin.communicraft.com:5000/php-5.3:apache apache/
+#docker build --rm -t docker.dublin.communicraft.com:5000/php-5.3:fpm fpm/
 
-docker build --rm -t cespi/php-5.3:modules-cli with-modules
-docker build --rm -t cespi/php-5.3:modules-apache with-modules/apache
-docker build --rm -t cespi/php-5.3:modules-fpm with-modules/fpm
+#docker build --rm -t docker.dublin.communicraft.com:5000/php-5.3:modules-cli with-modules
+#docker build --rm -t docker.dublin.communicraft.com:5000/php-5.3:modules-apache with-modules/apache
+#docker build --rm -t docker.dublin.communicraft.com:5000/php-5.3:modules-fpm with-modules/fpm
 
 if [ "$1" = "push" ]; then
-  for tag in cli apache fpm; do
-    docker push cespi/php-5.3:$tag
-    docker push cespi/php-5.3:modules-$tag
+#  for tag in cli apache fpm; do
+  for tag in apache; do
+    docker push docker.dublin.communicraft.com:5000/php-5.3:$tag
+    # docker push docker.dublin.communicraft.com:5000/php-5.3:modules-$tag
   done
 fi
